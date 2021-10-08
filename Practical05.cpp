958b236af9544ee98600e84041ba47dc8aa676cf
ssammu123 <91406488+ssammu123@users.noreply.github.com>
2021-10-08 12:15:27 +0530

    Add files via upload

---
+++ Practical05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "Size of different primary datatypes in c++"<<endl;
+  cout << "char :" << sizeof(char) << endl;
+  cout << "short :" << sizeof(short) << endl;
+  cout << "Int :" <<sizeof(int) << endl;
+  cout << "Long :" << sizeof(long int) << endl;
+  cout << "Double :" << sizeof(double) << endl;
+  cout << "Wide Character :"<< sizeof(wchar_t)<<endl;
+  return 0;
+ } 
---
+++ practial5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int age = 15;
+  char test1 = 'h';
+  float area = 64.74;
+  wchar_t test = L'd';
+  double distance = 435563;
+  bool cond = false;
+  
+  cout << "Int :"<< age << endl;
+  cout<< "char: "<< test1 << endl;
+  cout<< "float:"<< area << endl;
+  cout<< "Wchar: "<< test << endl;
+  cout<<"Double :" << distance << endl;
+  cout<< "booleab: " << cond << endl;
+  return 0;
+}
